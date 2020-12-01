74a64f27f2898429d5ec615a84548573ef44a0f5
jrialland <julien.rialland@gmail.com>
2020-12-01 14:10:33 +0100

    adapted to the new Musical class

--- src/autopitch/AutoPitchPlugin.cpp
+++ src/autopitch/AutoPitchPlugin.cpp
@@ -1,6 +1,5 @@
 #include "AutoPitchPlugin.hpp"
-#include "common-audio/music.hpp"
-using namespace music;
+#include "common-audio/Musical.hpp"
 
 #include <iostream>
 using namespace std;
@@ -28,20 +27,19 @@ AutoPitchPlugin::~AutoPitchPlugin() {
 void AutoPitchPlugin::activate() { correction = 1.0; }
 
 void AutoPitchPlugin::run(uint32_t nsamples) {
+
   float *input = get_buffer<float>(0);
   float *output = get_buffer<float>(1);
-  cout << "input " << input << endl;
-  cout << "output " << output << endl;
-
-  ScaleInKey sk(Temperament::Equal, Scale::Ionian, Key::F);
 
   float pitch = pitchDetector->detect_pitch(nsamples, input);
 
   if (pitch > 0) {
     // find the nearest note that is in the current scale
-    Note target = sk.nearest_note(pitch);
-    correction = target.freq() / pitch;
+    Musical::Note target =
+        Musical::getNearestNote("Minor pentatonic", "A", pitch);
+    correction = target.freq / pitch;
   }
+
   pitchShifter->shift(nsamples, input, output, correction);
 }
 
