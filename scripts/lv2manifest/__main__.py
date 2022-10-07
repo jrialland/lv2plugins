307447a8df7c2f8a4be7c36df6cf56e83ae8be8c
Julien Rialland <julien.rialland@altran.com>
2022-10-07 17:01:34 +0200

    customized logs

--- scripts/lv2manifest/__main__.py
+++ scripts/lv2manifest/__main__.py
@@ -1,22 +1,49 @@
-
 from .model import Plugin, PluginType, generate_audio_plugin_desc, generate_manifest
 from .metacomments import scan_directory
 import sys
 import json
 import os
+from .logging import configure_logging
+import logging
 
-if __name__ == '__main__':
-    builddir = sys.argv[1]
-    for uri, meta in scan_directory('../src').items():
+if __name__ == "__main__":
+    configure_logging()
+    if len(sys.argv) != 2:
+        builddir = os.path.realpath(
+            os.path.join(os.path.dirname(__file__), "..", "..", "build")
+        )
+    else:
+        builddir = sys.argv[1]
+    logging.debug(f"builddir : {builddir}")
+    sourcedir = os.path.join(builddir, "..", "src")
+    logging.debug(f"sourcedir : {sourcedir}")
+    found_annotations = scan_directory(sourcedir)
+    if not found_annotations:
+        logging.warning("0 annotations were found")
+
+    # for every plugin detected in the sources
+    for uri, meta in found_annotations.items():
+        # parse the json in order to have an object
         plugin = Plugin.from_json(json.dumps(meta))  # type:ignore
-        targetdir = os.path.join(builddir, f'{plugin.name}.lv2')
+        logging.info(f"Detected Plugin : {plugin.name} ( {plugin.uri} )")
+        # where the files will be generated
+        targetdir = os.path.join(builddir, f"{plugin.name}.lv2")
         if not os.path.isdir(targetdir):
             os.makedirs(targetdir)
-        with open(os.path.join(targetdir, 'manifest.ttl'), 'wb') as f:
+
+        # write a "general" manifest file
+
+        manifestttl = os.path.join(targetdir, "manifest.ttl")
+        logging.info(f"  - writing {manifestttl}")
+        with open(manifestttl, "wb") as f:
             g = generate_manifest(plugin)
-            g.serialize(destination=f, format='turtle')
+            g.serialize(destination=f, format="turtle")
+
+        # for audio plugins
 
         if plugin.pluginType is PluginType.AUDIO:
-            with open(os.path.join(targetdir, f'{plugin.name}.ttl'), 'wb') as f:
+            pluginttl = os.path.join(targetdir, f"{plugin.name}.ttl")
+            logging.info(f"  - writing {pluginttl}")
+            with open(pluginttl, "wb") as f:
                 g = generate_audio_plugin_desc(plugin)
-                g.serialize(destination=f, format='turtle')
+                g.serialize(destination=f, format="turtle")
